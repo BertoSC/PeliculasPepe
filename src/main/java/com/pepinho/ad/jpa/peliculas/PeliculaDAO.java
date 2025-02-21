16a17476ee9e92c8bde7a92df4bbfa3c13a11b24
A23AlbertoGC <a23albertogc@iessanclemente.net>
2025-02-21 10:42:46 +0100

    - PeliculaDTO, PeliculaPaginaDTO empezado

---
+++ src/main/java/com/pepinho/ad/jpa/peliculas/PeliculaDAO.java
@@ -0,0 +1,7 @@
+package com.pepinho.ad.jpa.peliculas;
+
+public class PeliculaDAO {
+
+    // Crea una clase PeliculaDAO que tenga un método que devuelva el número total de películas
+    // y otro que devuelva las lista de películas de una página concreta, ordenadas por año descendente.
+}
---
+++ src/main/java/com/pepinho/ad/jpa/peliculas/PeliculaPaginaDTO.java
@@ -0,0 +1,5 @@
+package com.pepinho.ad.jpa.peliculas;
+
+public class PeliculaPaginaDTO {
+    // idPelicula, castelan, orixinal, anoFin y director.
+}
